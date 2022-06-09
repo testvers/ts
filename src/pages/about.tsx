36746c0dc7dfda6bc660377c0f39328caa70a73c
testvers <87796576+testvers@users.noreply.github.com>
2022-06-09 08:23:29 +0500

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -36,7 +36,7 @@ const AboutUs: FC = () => {
       <Typography variant="h2" gutterBottom component="h1">
         About Us
       </Typography>
-{*/
+{/*
       <Typography variant="h4">Members:</Typography>
       <div className={classed.root}>
         <Grid container spacing={2}>
