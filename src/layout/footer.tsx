b8fb9eb4e63fca54edfd4fbd18c35aaf1c147e90
testvers <87796576+testvers@users.noreply.github.com>
2022-06-10 00:07:52 +0500

    Update footer.tsx

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -4,7 +4,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
 
 function Copyright() {
   return (
